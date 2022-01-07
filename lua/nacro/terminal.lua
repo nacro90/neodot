413d983648d7268039a18310643d49300b72475c
nacro90 <orcan.tiryakioglu@gmail.com>
2022-01-07 10:17:24 +0300

    Improve terminal config

--- lua/nacro/terminal.lua
+++ lua/nacro/terminal.lua
@@ -1,10 +1,17 @@
 local terminal = {}
 
+local opt = vim.opt
+
+function terminal.on_term_open()
+  opt.sidescrolloff = 0
+  opt.scrollback = 50000
+end
+
 function terminal.setup()
   vim.cmd [[
     augroup terminal_setup
       autocmd!
-      autocmd TermOpen term://* setlocal sidescrolloff=0
+      autocmd TermOpen term://* lua require("nacro.terminal").on_term_open()
     augroup END
   ]]
 end
