b05f2b97dbb6603383bcc16bca958f94c66db234
nacro90 <orcan.tiryakioglu@gmail.com>
2022-01-12 09:57:47 +0300

    Improve zettelkasten

--- lua/nacro/zettelkasten.lua
+++ lua/nacro/zettelkasten.lua
@@ -8,16 +8,28 @@ local keymap = vim.keymap
 
 zk.dir = vim.env.HOME .. "/Zettels"
 
+local function inserter(func)
+  return function()
+    func { insert = true }
+  end
+end
+
 function zk.setup()
   neuron.setup {
     neuron_dir = zk.dir,
-    mappings = false,
+    leader = "<leader>z",
   }
 
-  keymap.set("n", "<leader>z", n_telescope.find_zettels)
-  keymap.set("n", "<leader>Z", function()
-    n_cmd.new_edit(zk.dir)
-  end)
+  local cmd_str = [[
+    augroup nacro_neuron_setup
+      autocmd!
+      autocmd BufRead,BufNew %s/*.md lua require("neuron.mappings").set_keymaps()
+    augroup END
+  ]]
+  vim.cmd(cmd_str:format(zk.dir))
+
+  keymap.set("n", "<leader>zz", n_telescope.find_zettels)
+  keymap.set("n", "<leader>zZ", inserter(n_telescope.find_zettels))
 end
 
 return zk
