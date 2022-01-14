5af44ba5624350ebc935f0e07c4add43437de953
nacro90 <orcan.tiryakioglu@gmail.com>
2022-01-14 08:21:00 +0300

    Change markdown config to lua

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,24 @@
+local opt_local = vim.opt_local
+local keymap = vim.keymap
+
+opt_local.autoindent = true
+opt_local.shiftwidth = 2
+opt_local.tabstop = 2
+opt_local.expandtab = true
+opt_local.conceallevel = 2
+opt_local.textwidth = 100
+opt_local.colorcolumn = 101
+opt_local.foldenable = true
+opt_local.spelllang = { "en", "tr" }
+opt_local.wrap = true
+opt_local.linebreak = true
+
+keymap.set("n", "<leader>x", "<Cmd>normal! 0f[lrX<CR>", {buffer = true})
+
+--[[
+set iskeyword+=-
+set formatoptions=tlnqr
+
+" Mappings
+" autocmd! BufWritePre *.md,*.mkd,*.markdown substitute
+]]--
