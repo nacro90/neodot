96c07dc1b08deaf46032a00ddcca8dcac1c6f43c
nacro90 <orcan.tiryakioglu@gmail.com>
2022-01-25 20:43:22 +0300

    Personalize zettel workflow

--- lua/nacro/zettelkasten/init.lua
+++ lua/nacro/zettelkasten/init.lua
@@ -4,6 +4,8 @@ local neuron = require "neuron"
 local n_telescope = require "neuron.telescope"
 local n_cmd = require "neuron.cmd"
 
+local builtin = require "telescope.builtin"
+
 local keymap = vim.keymap
 
 zk.dir = vim.env.HOME .. "/Zettels"
@@ -12,6 +14,26 @@ local function inserter(func)
   return function()
     func { insert = true }
   end
+end
+
+function zk.find_zettel_by_filename(zk_dir)
+  zk_dir = zk_dir or zk.dir
+  builtin.find_files { cwd = zk_dir }
+end
+
+function zk.new_zettel(name, zk_dir)
+  zk_dir = zk_dir or zk.dir
+  if not name then
+    vim.ui.input({ prompt = "New zettel: " }, function(inp)
+      name = inp
+    end)
+  end
+
+  if not name then
+    return
+  end
+
+  vim.cmd("edit " .. zk.dir .. "/" .. name)
 end
 
 function zk.setup()
@@ -28,7 +50,8 @@ function zk.setup()
   ]]
   vim.cmd(cmd_str:format(zk.dir))
 
-  keymap.set("n", "<leader>ez", n_telescope.find_zettels)
+  keymap.set("n", "<leader>ez", zk.find_zettel_by_filename)
+  keymap.set("n", "<leader>zi", inserter(n_telescope.find_zettels))
   keymap.set("n", "<leader>zi", inserter(n_telescope.find_zettels))
 end
 
