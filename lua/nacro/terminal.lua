c0f0545a69f8adcf0df36a69b844c650b2590476
nacro90 <orcan.tiryakioglu@gmail.com>
2022-05-28 13:29:36 +0300

    Improve terminal config

--- lua/nacro/terminal.lua
+++ lua/nacro/terminal.lua
@@ -1,19 +1,27 @@
 local terminal = {}
 
-local opt = vim.opt
+local opt_local = vim.opt_local
+local api = vim.api
 
-function terminal.on_term_open()
-  opt.sidescrolloff = 0
-  opt.scrollback = 50000
+local function on_term_open()
+  opt_local.sidescrolloff = 0
+  opt_local.scrollback = 50000
+  opt_local.number = false
+  opt_local.relativenumber = false
+  vim.cmd "startinsert"
+end
+
+local function create_autocmds()
+  local group = api.nvim_create_augroup("terminal_setup", {})
+  api.nvim_create_autocmd("TermOpen", {
+    group = group,
+    pattern = "term://*",
+    callback = on_term_open,
+  })
 end
 
 function terminal.setup()
-  vim.cmd [[
-    augroup terminal_setup
-      autocmd!
-      autocmd TermOpen term://* lua require("nacro.terminal").on_term_open()
-    augroup END
-  ]]
+  create_autocmds()
 end
 
 return terminal
