82860979d2ddbf209544fa648243f09d5fd84ee1
nacro90 <orcan.tiryakioglu@gmail.com>
2022-06-17 09:08:59 +0300

    Remove neorg

--- lua/nacro/plugins.lua
+++ lua/nacro/plugins.lua
@@ -136,27 +136,6 @@ local plugin_table = {
     end,
     cmd = "MaximizerToggle",
   },
-  {
-    "nvim-neorg/neorg",
-    config = function()
-      require("neorg").setup {
-        -- Tell Neorg what modules to load
-        load = {
-          ["core.defaults"] = {}, -- Load all the default modules
-          ["core.norg.concealer"] = {}, -- Allows for use of icons
-          ["core.norg.dirman"] = { -- Manage your directories with Neorg
-            config = {
-              workspaces = {
-                my_workspace = "~/neorg",
-              },
-            },
-          },
-        },
-      }
-    end,
-    requires = "nvim-lua/plenary.nvim",
-    disable = false,
-  },
   "TimUntersberger/neogit",
   {
     -- Easy text exchange operator for Vim
