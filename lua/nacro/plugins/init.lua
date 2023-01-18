a51029e53da6f0ee92f5284aa6d9f76572850045
nacro90 <orcan.tiryakioglu@gmail.com>
2023-01-18 11:20:21 +0300

    Fix colorscheme

--- lua/nacro/plugins/init.lua
+++ lua/nacro/plugins/init.lua
@@ -27,7 +27,8 @@ return {
   },
   {
     "folke/tokyonight.nvim",
-    lazy = true,
+    lazy = false,
+    priority = 1000,
     config = function()
       vim.cmd "colorscheme tokyonight"
     end,
