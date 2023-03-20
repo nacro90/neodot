d1041a5e5687e62bce3699edf1340a1b8de580bf
nacro90 <orcan.tiryakioglu@gmail.com>
2023-03-20 12:22:18 +0300

    Add trailing whitespace remover for norg ftplugin

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -3,3 +3,11 @@ vim.opt_local.expandtab = true
 vim.opt_local.conceallevel = 1
 vim.opt_local.foldlevel = 1
 vim.opt_local.foldnestmax = 1
+
+local group = vim.api.nvim_create_augroup("FTPluginNorg", {})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = group,
+  pattern = "*.norg",
+  command = "RemoveTrailingWhitespace",
+})
