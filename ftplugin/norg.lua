175f288ac9cc7326a91806c981d2974e25eaa6d5
nacro90 <orcan.tiryakioglu@gmail.com>
2023-04-19 18:55:29 +0300

    Improve norg

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -2,6 +2,7 @@ vim.opt_local.expandtab = true
 vim.opt_local.conceallevel = 1
 vim.opt_local.foldlevel = 1
 vim.opt_local.foldnestmax = 1
+vim.opt_local.wrap = true
 
 local group = vim.api.nvim_create_augroup("FTPluginNorg", {})
 
