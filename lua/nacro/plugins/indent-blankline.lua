1c2d382aaac3f66f74ae18045d3ba9a062ebd07b
nacro90 <orcan.tiryakioglu@gmail.com>
2023-10-27 19:02:05 +0300

    Fix indent blankline

--- lua/nacro/plugins/indent-blankline.lua
+++ lua/nacro/plugins/indent-blankline.lua
@@ -1,23 +1,12 @@
-local function config()
-  require("indent_blankline").setup {
-    char = "·",
-    show_trailing_blankline_indent = false,
-    max_indent_increase = 2,
-    char_highlight_list = {
-      "IndentBlanklineChar",
-    },
-    indent_blankline_filetype_exclude = {
-      "norg",
-    },
-    show_current_context = true,
-    context_char = "·",
-    context_highlight_list = {
-      "CursorLineNr",
-    },
-  }
-end
-
 return {
   "lukas-reineke/indent-blankline.nvim",
-  config = config,
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "▏",
+    },
+    scope = {
+      enabled = false,
+    },
+  },
 }
