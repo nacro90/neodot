e122d6621372c96f07fca38c00455248f178d000
nacro90 <orcan.tiryakioglu@gmail.com>
2024-01-22 17:13:22 +0300

    Fix rest nvim

--- lua/nacro/plugins/rest-nvim.lua
+++ lua/nacro/plugins/rest-nvim.lua
@@ -3,12 +3,12 @@ return {
   ft = "http",
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
+    stay_in_current_window_after_split = true,
     result = {
       show_url = true,
       show_curl_command = true,
       show_http_info = true,
       show_headers = true,
-      show_statistics = true,
       formatters = {
         json = "jq",
         html = function(body)
