2cbf8e17a8d2d33e31fa28eebee5a70047329b79
nacro90 <orcan.tiryakioglu@gmail.com>
2024-01-31 13:28:10 +0300

    Fix daily notes location obsidian

--- lua/nacro/plugins/obsidian.lua
+++ lua/nacro/plugins/obsidian.lua
@@ -19,6 +19,14 @@ return {
       title = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
       return title
     end,
+    daily_notes = {
+      folder = "daily",
+      -- date_format = "%Y-%m-%d",
+      -- -- Optional, if you want to change the date format of the default alias of daily notes.
+      -- alias_format = "%B %-d, %Y",
+      -- -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      -- template = nil
+    },
   },
   config = function(_, opts)
     local obsidian = require "obsidian"
