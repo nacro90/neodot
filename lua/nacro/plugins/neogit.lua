32a9b8a03fc71e57bad1134def1d6c862af409e7
nacro90 <orcan.tiryakioglu@gmail.com>
2024-02-26 11:19:42 +0300

    Improve neogit

--- lua/nacro/plugins/neogit.lua
+++ lua/nacro/plugins/neogit.lua
@@ -1,10 +1,14 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim", 
-    "sindrets/diffview.nvim", 
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
     "nvim-telescope/telescope.nvim",
     "ibhagwan/fzf-lua",
   },
   opts = {},
+  keys = {
+    { "<leader>G", "<Cmd>Neogit<CR>" },
+  },
+  cmd = "Neogit",
 }
