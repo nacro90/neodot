eb32940334142a9713b68edb446e127a6c712304
nacro90 <orcan.tiryakioglu@gmail.com>
2024-04-19 13:09:16 +0300

    Improve neogit

--- lua/nacro/plugins/neogit.lua
+++ lua/nacro/plugins/neogit.lua
@@ -6,7 +6,17 @@ return {
     "nvim-telescope/telescope.nvim",
     "ibhagwan/fzf-lua",
   },
-  opts = {},
+  opts = {
+    auto_show_console = false,
+    graph_style = "unicode",
+    integrations = {
+      diffview = true,
+    },
+    signs = {
+      item = { "▪", "-" },
+      section = { "▪", "-" },
+    },
+  },
   keys = {
     { "<leader>G", "<Cmd>Neogit<CR>" },
   },
