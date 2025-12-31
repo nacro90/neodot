58eac842064cbe4dcac07d8f1165f309814c81f3
nacro90 <orcan.tiryakioglu@gmail.com>
2025-12-31 04:21:42 +0300

    Clean up options.lua and remove Neovim defaults

--- lua/nacro/options.lua
+++ lua/nacro/options.lua
@@ -1,32 +1,70 @@
 local g = vim.g
 local opt = vim.opt
 
-vim.cmd "filetype plugin indent on"
+-- Leader key (must be set before lazy.nvim)
+g.mapleader = " "
 
-opt.termguicolors = true
-opt.syntax = "enable"
+-- UI
 opt.cursorline = true
 opt.number = true
 opt.relativenumber = true
-opt.hidden = true
 opt.wrap = false
 opt.linebreak = true
 opt.splitbelow = true
 opt.splitright = true
 opt.showmode = false
-opt.showcmd = true
 opt.visualbell = true
 opt.signcolumn = "auto:2"
 opt.title = true
-opt.list = true
 opt.pumheight = 12
 opt.scrolloff = 1
 opt.sidescrolloff = 5
-opt.laststatus = 2
+opt.cmdheight = 0
+opt.showbreak = "\\"
+opt.breakindent = true
+opt.breakindentopt = {
+  "sbr",
+  shift = "2",
+  min = "20",
+}
+opt.fillchars = {
+  vert = "┃",
+  diff = " ",
+  foldsep = "│",
+  foldopen = "┍",
+  foldclose = "≡",
+}
+
+-- List characters
+opt.list = true
+opt.listchars = {
+  tab = "  ",
+  trail = "_",
+  extends = ">",
+  precedes = "<",
+  nbsp = "+",
+}
+
+-- Editing
 opt.clipboard = "unnamedplus"
-opt.wildmenu = true
-opt.wildmode = { "longest", "full" }
 opt.fileformat = "unix"
+opt.smartindent = true
+opt.expandtab = true
+opt.shiftround = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.virtualedit = "onemore"
+opt.formatoptions = "tcqj"
+opt.nrformats = { "bin", "hex" }
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.inccommand = "nosplit"
+
+-- Completion
+opt.wildmode = { "longest", "full" }
 opt.wildignore = {
   "*.swp",
   "*.bak",
@@ -35,21 +73,13 @@ opt.wildignore = {
   "*.hi",
   "*.o",
 }
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Folding
 opt.foldlevel = 99
-opt.timeout = false
 opt.foldcolumn = "0"
-opt.mouse = "a"
-opt.ignorecase = true
-opt.smartcase = true
-opt.smartindent = true
-opt.inccommand = "nosplit"
-opt.expandtab = true
-opt.shiftround = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.undofile = true
-opt.nrformats = { "bin", "hex" }
+
+-- Session & View
 opt.sessionoptions = {
   "blank",
   "buffers",
@@ -69,49 +99,24 @@ opt.viewoptions = {
   "unix",
 }
 opt.switchbuf = "usetab"
--- See https://github.com/hrsh7th/nvim-cmp
-opt.completeopt = { "menu", "menuone", "noselect" }
-opt.list = true
-opt.listchars = {
-  tab = "  ",
-  trail = "_",
-  extends = ">",
-  precedes = "<",
-  nbsp = "+",
-}
-opt.formatoptions = "tcqj"
-opt.showbreak = "\\"
-opt.errorbells = false
-opt.cmdheight = 0
-opt.swapfile = false
+
+-- Performance & Behavior
+opt.timeoutlen = 500
 opt.updatetime = 500
-opt.virtualedit = "onemore"
+opt.undofile = true
+opt.swapfile = false
+opt.errorbells = false
+opt.mouse = "a"
 opt.shortmess:append "c"
 opt.shortmess:append "S"
 opt.shortmess:append "W"
-opt.lazyredraw = true
-opt.fillchars = {
-  vert = "┃",
-  diff = " ",
-  foldsep = "│",
-  foldopen = "┍",
-  foldclose = "≡",
-}
-opt.breakindent = true
-opt.breakindentopt = {
-  "sbr",
-  shift = "2",
-  min = "20",
-}
 
-g.mapleader = " "
-g.loaded_netrwPlugin = 1
-
+-- Filetype specific globals
 g.ch_syntax_for_h = 1
 g.python_highlight_all = 1
 g.python_recommended_style = 0
 
--- Macos language bugfix
+-- macOS language bugfix
 if require("nacro.os").get_name() == "mac" then
   vim.cmd "language en_US.UTF-8"
 end
