7a6068d2387ead289653928c49bc3354d16e669b
nacro90 <orcan.tiryakioglu@gmail.com>
2026-01-02 04:49:06 +0300

    Improve java ftplugin: lazy load telescope and add descriptions

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,14 +1,14 @@
-local telescope = require "telescope"
-local builtin = require "telescope.builtin"
-
+-- Java-specific telescope overrides with lazy loading
 vim.keymap.set("n", "<leader>N", function()
-  builtin.find_files { hidden = true, no_ignore = true }
+  require("telescope.builtin").find_files { hidden = true, no_ignore = true }
 end, {
   buffer = 0,
+  desc = "Find all files (Java)",
 })
 
 vim.keymap.set("n", "<leader>n", function()
-  builtin.find_files { path_display = { "tail" } }
+  require("telescope.builtin").find_files { path_display = { "tail" } }
 end, {
   buffer = 0,
+  desc = "Find files (tail display)",
 })
